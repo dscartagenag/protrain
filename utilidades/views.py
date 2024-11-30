5b570986ca29429fd8b0425693111a5a71e1cde0
Alvaro Contreras <alvandconros@gmail.com>
2024-11-29 20:00:59 -0500

    Se ajusta la funcion crear_generar codigo qr
     En la rama utilidades
     Cambios a ser confirmados:
    	modificados:     .gitignore
    	nuevos archivos: utilidades/img_qr/init.txt
    	modificados:     utilidades/views.py

--- utilidades/views.py
+++ utilidades/views.py
@@ -1,33 +1,55 @@
 import qrcode
-from django.http import HttpResponse
-from django.shortcuts import render
-from io import BytesIO
+from PIL import Image
+import os
 
 
-def generar_qr(request):    
-    data = request.GET.get('data', 'protrain la mejor opcion')
+def generar_codigo_qr(texto_convertir, nombre_guardar):
+    """
+    Genera un código QR a partir del texto proporcionado y lo guarda como una imagen.
 
-    qr = qrcode.QRCode(
-        version=1,  # Controla el tamaño del código QR
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
+    Args:
+        texto_convertir (str): El texto que se convertirá en un código QR.
+        nombre_guardar (str): El nombre del archivo en el que se guardará la imagen del código QR.
 
-    # Genera una imagen del código QR
-    img = qr.make_image(fill='black', back_color='white')
+    Returns:
+        bool: True si el código QR se genera y guarda correctamente, False en caso contrario.
+    """
+    try:
+        # Crear un objeto QRCode
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
 
-    # Convierte la imagen a un formato que pueda ser enviado en la respuesta HTTP
-    buffer = BytesIO()
-    img.save(buffer, format='PNG')
-    img_str = buffer.getvalue()
+        # Agregar datos al objeto QRCode
+        qr.add_data(texto_convertir)
+        qr.make(fit=True)
 
-    # Retorna la imagen como una respuesta HTTP
-    return HttpResponse(img_str, content_type='image/png')
+        # Crear la imagen del código QR
+        img = qr.make_image(fill='black', back_color='white')
 
-    
+        # Guardar la imagen con el nombre especificado
+        ruta_archivo = os.path.join('utilidades/img_qr', nombre_guardar)
+        img.save(ruta_archivo)
+
+        return True
+    except Exception as e:
+        # Si ocurre un error, imprimir el error y retornar False
+        print(f"Error al generar el código QR: {e}")
+        return False
+
+
+if __name__ == "__main__":
+    texto = "https://www.pamec.com.co"
+    nombre_archivo = "ir_pamec.png"
+
+    if generar_codigo_qr(texto, nombre_archivo):
+        print("Código QR generado y guardado correctamente.")
+    else:
+        print("Hubo un error al generar el código QR.")
+
     
 
 
