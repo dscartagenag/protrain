7fa4875102b69dbac5b5afd63ea81a4dcec8c44f
Alvaro Contreras <alvandconros@gmail.com>
2024-12-12 16:43:29 -0500

    Se soluciona bug
     En la rama main
     Tu rama está actualizada con 'origin/main'.
    
     Cambios a ser confirmados:
    	modificados:     utilidades/urls.py

--- utilidades/urls.py
+++ utilidades/urls.py
@@ -1,8 +1,8 @@
-from .views import generar_qr
+from .views import generar_codigo_qr
 from django.urls import path
 
 app_name = 'utilidades'
 
 urlpatterns = [
-    path('generar_qr/', generar_qr, name='index'), # urls  utilidades: index
+    path('generar_qr/', generar_codigo_qr, name='index'), # urls  utilidades: index
 ]
